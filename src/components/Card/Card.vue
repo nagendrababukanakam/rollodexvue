73d4107dcfb1f86cbdbd67263dc3b2152ba5c6ad
Nagendra Babu Kanakam <nagendra.kanakam@qentelli.com>
2022-02-03 10:13:54 +0530

    home and cardlist

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,32 @@
+<template>
+<div className='card-container'>
+
+    <img
+    alt='monster'
+    src='https://robohash.org/1?set=set2&size=180x180'
+    />
+    <h2>Nagendra</h2>
+
+    <p>Nagendra@gmail.com</p>
+
+   </div>
+</template>
+
+<script>
+</script>
+
+<styles>
+.card-container{
+  display: flex;
+  flex-direction: column;
+  background-color: #95dada;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+  -moz-osx-font-smoothing: grayscale;
+  backface-visibility: hidden;
+  transform: translateZ(0);
+  transition: transform 0.25s ease-out;
+}
+</styles>
---
+++ src/components/Search/Search.vue
@@ -0,0 +1,29 @@
+<template>
+<input
+        name='search'
+        className='search-box'
+        type='password'
+        placeholder='search monsters'
+        onChange = {props.handleSearch}
+    />
+</template>
+
+<script>
+export default {
+components: {
+ComponentA
+}
+}
+</script>
+
+<styles>
+.search-box {
+    -webkit-appearance: none;
+    border: none;
+    outline: none;
+    padding: 10px;
+    width: 150px;
+    line-height: 30px;
+    margin-bottom: 30px;
+  }
+</styles>
